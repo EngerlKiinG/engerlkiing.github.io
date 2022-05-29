27b61f8cc6b8ecf803af03b13c04fe05750cffa7
EngerlKiinG <106326785+EngerlKiinG@users.noreply.github.com>
2022-05-29 13:40:04 -0400

    Commit Changes

---
+++ css1/js/main.js
@@ -0,0 +1,10 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelector('pelicula');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-Derecha');
+
+//-------- ------------ Event Listener para la flecha derecha ---------- -----------//
+flechaDerecha.addEventListener('click', () => {
+    
+});
